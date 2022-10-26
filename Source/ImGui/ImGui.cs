173b946f69f6e70f53a08eed919575a154b5bd6d
Wojciech Figat <wojtek@figat.pl>
2022-10-26 16:04:10 +0200

    Add C# API

---
+++ Source/ImGui/ImGui.cs
@@ -0,0 +1,224 @@
+﻿// Copyright (c) Wojciech Figat. All rights reserved.
+
+using FlaxEngine;
+
+/// <summary>
+/// A cardinal direction
+/// </summary>
+public enum ImGuiDir
+{
+    /// <summary>
+    /// Nothing
+    /// </summary>
+    None = -1,
+
+    /// <summary>
+    /// Left
+    /// </summary>
+    Left = 0,
+
+    /// <summary>
+    /// Right
+    /// </summary>
+    Right = 1,
+
+    /// <summary>
+    /// Up
+    /// </summary>
+    Up = 2,
+
+    /// <summary>
+    /// Down
+    /// </summary>
+    Down = 3,
+};
+
+/// <summary>
+/// Flags for DragFloat(), DragInt(), SliderFloat(), SliderInt() etc.
+/// </summary>
+public enum ImGuiSliderFlags
+{
+    /// <summary>
+    /// Nothing
+    /// </summary>
+    None = 0,
+
+    /// <summary>
+    /// Clamp value to min/max bounds when input manually with CTRL+Click. By default CTRL+Click allows going out of bounds.
+    /// </summary>
+    AlwaysClamp = 1 << 4,
+
+    /// <summary>
+    /// Make the widget logarithmic (linear otherwise). Consider using ImGuiSliderFlags_NoRoundToFormat with this if using a format-string with small amount of digits.
+    /// </summary>
+    Logarithmic = 1 << 5,
+
+    /// <summary>
+    /// Disable rounding underlying value to match precision of the display format string (e.g. %.3f values are rounded to those 3 digits)
+    /// </summary>
+    NoRoundToFormat = 1 << 6,
+
+    /// <summary>
+    /// Disable CTRL+Click or Enter key allowing to input text directly into the widget
+    /// </summary>
+    NoInput = 1 << 7,
+};
+
+/// <summary>
+/// Flags for InputText().
+/// </summary>
+public enum ImGuiInputTextFlags
+{
+    /// <summary>
+    /// Nothing.
+    /// </summary>
+    None = 0,
+
+    /// <summary>
+    /// Allow 0123456789.+-*/
+    /// </summary>
+    CharsDecimal = 1 << 0,
+
+    /// <summary>
+    /// Allow 0123456789ABCDEFabcdef
+    /// </summary>
+    CharsHexadecimal = 1 << 1,
+
+    /// <summary>
+    /// Turn a..z into A..Z
+    /// </summary>
+    CharsUppercase = 1 << 2,
+
+    /// <summary>
+    /// Filter out spaces, tabs
+    /// </summary>
+    CharsNoBlank = 1 << 3,
+
+    /// <summary>
+    /// Select entire text when first taking mouse focus
+    /// </summary>
+    AutoSelectAll = 1 << 4,
+
+    /// <summary>
+    /// Return 'true' when Enter is pressed (as opposed to every time the value was modified). Consider looking at the IsItemDeactivatedAfterEdit() function.
+    /// </summary>
+    EnterReturnsTrue = 1 << 5,
+
+    /// <summary>
+    /// Callback on pressing TAB (for completion handling)
+    /// </summary>
+    CallbackCompletion = 1 << 6,
+
+    /// <summary>
+    /// Callback on pressing Up/Down arrows (for history handling)
+    /// </summary>
+    CallbackHistory = 1 << 7,
+
+    /// <summary>
+    /// Callback on each iteration. User code may query cursor position, modify text buffer.
+    /// </summary>
+    CallbackAlways = 1 << 8,
+
+    /// <summary>
+    /// Callback on character inputs to replace or discard them. Modify 'EventChar' to replace or discard, or return 1 in callback to discard.
+    /// </summary>
+    CallbackCharFilter = 1 << 9,
+
+    /// <summary>
+    /// Pressing TAB input a '\t' character into the text field
+    /// </summary>
+    AllowTabInput = 1 << 10,
+
+    /// <summary>
+    /// In multi-line mode, unfocus with Enter, add new line with Ctrl+Enter (default is opposite: unfocus with Ctrl+Enter, add line with Enter).
+    /// </summary>
+    CtrlEnterForNewLine = 1 << 11,
+
+    /// <summary>
+    /// Disable following the cursor horizontally
+    /// </summary>
+    NoHorizontalScroll = 1 << 12,
+
+    /// <summary>
+    /// Overwrite mode
+    /// </summary>
+    AlwaysOverwrite = 1 << 13,
+
+    /// <summary>
+    /// Read-only mode
+    /// </summary>
+    ReadOnly = 1 << 14,
+
+    /// <summary>
+    /// Password mode, display all characters as '*'
+    /// </summary>
+    Password = 1 << 15,
+
+    /// <summary>
+    /// Disable undo/redo. Note that input text owns the text data while active, if you want to provide your own undo/redo stack you need e.g. to call ClearActiveID().
+    /// </summary>
+    NoUndoRedo = 1 << 16,
+
+    /// <summary>
+    /// Allow 0123456789.+-*/eE (Scientific notation input)
+    /// </summary>
+    CharsScientific = 1 << 17,
+
+    /// <summary>
+    /// Callback on buffer capacity changes request (beyond 'buf_size' parameter value), allowing the string to grow. Notify when the string wants to be resized (for string types which hold a cache of their Size). You will be provided a new BufSize in the callback and NEED to honor it. (see misc/cpp/imgui_stdlib.h for an example of using this)
+    /// </summary>
+    CallbackResize = 1 << 18,
+
+    /// <summary>
+    /// Callback on any edit (note that InputText() already returns true on edit, the callback is useful mainly to manipulate the underlying buffer while focus is active)
+    /// </summary>
+    CallbackEdit = 1 << 19,
+};
+
+partial class ImGui
+{
+    /// <summary>
+    /// Button.
+    /// </summary>
+    [Unmanaged]
+    public static bool Button(string label)
+    {
+        return Button(label, Float2.Zero);
+    }
+
+    /// <summary>
+    /// Image.
+    /// </summary>
+    [Unmanaged]
+    public static void Image(GPUTexture texture, Float2 size)
+    {
+        Image(texture, size, Float2.Zero, Float2.One, Color.White, Color.Transparent);
+    }
+
+    /// <summary>
+    /// less than 0 framePadding uses default frame padding settings. 0 for no padding.
+    /// </summary>
+    [Unmanaged]
+    public static bool ImageButton(GPUTexture texture, Float2 size)
+    {
+        return ImageButton(texture, size, Float2.Zero, Float2.One, Color.White, Color.Transparent);
+    }
+
+    /// <summary>
+    /// ProgressBar.
+    /// </summary>
+    [Unmanaged]
+    public static void ProgressBar(float fraction)
+    {
+        ProgressBar(fraction, new Float2(-1, 0));
+    }
+
+    /// <summary>
+    /// Input multi-line text field.
+    /// </summary>
+    [Unmanaged]
+    public static bool InputTextMultiline(string label, ref string str, ImGuiInputTextFlags flags = 0)
+    {
+        return InputTextMultiline(label, ref str, new Float2(0, 0), flags);
+    }
+}
---
+++ Source/ImGui/ImGuiAPI.h
@@ -0,0 +1,335 @@
+﻿// Copyright (c) Wojciech Figat. All rights reserved.
+
+#pragma once
+
+#include "Engine/Core/Math/Vector2.h"
+#include "Engine/Core/Math/Vector3.h"
+#include "Engine/Core/Math/Vector4.h"
+#include "Engine/Core/Math/Color.h"
+#include "Engine/Core/Types/Span.h"
+#include "Engine/Core/Collections/Array.h"
+#include "Engine/Core/Types/String.h"
+#include "Engine/Scripting/ScriptingType.h"
+#include "ImGui/imgui.h"
+
+class GPUTexture;
+
+/// <summary>
+/// Dear ImGui interface that adds debug GUI interface to game viewport.
+/// </summary>
+API_CLASS(Namespace="", Name="ImGui", Static) class IMGUI_API ImGuiAPI
+{
+    DECLARE_SCRIPTING_TYPE_NO_SPAWN(ImGuiAPI);
+
+public:
+    // Separator, generally horizontal. inside a menu bar or in horizontal layout mode, this becomes a vertical separator.
+    API_FUNCTION() static void Separator()
+    {
+        ImGui::Separator();
+    }
+
+    // Call between widgets or groups to layout them horizontally. X position given in window coordinates.
+    API_FUNCTION() static void SameLine(float offsetFromStartX = 0.0f, float spacing = -1.0f)
+    {
+        ImGui::SameLine(offsetFromStartX, spacing);
+    }
+
+    // Undo a SameLine() or force a new line when in an horizontal-layout context.
+    API_FUNCTION() static void NewLine()
+    {
+        ImGui::NewLine();
+    }
+
+    // Add vertical spacing.
+    API_FUNCTION() static void Spacing()
+    {
+        ImGui::Spacing();
+    }
+
+    // Add a dummy item of given size. unlike InvisibleButton(), Dummy() won't take the mouse click or be navigable into.
+    API_FUNCTION() static void Dummy(const Float2& size)
+    {
+        ImGui::Dummy(*(ImVec2*)&size);
+    }
+
+    // Move content position toward the right, by indent_w, or style.IndentSpacing if indent_w less or equal to 0.
+    API_FUNCTION() static void Indent(float indentW = 0.0f)
+    {
+        ImGui::Indent(indentW);
+    }
+
+    // Move content position back to the left, by indent_w, or style.IndentSpacing if indent_w less or equal to 0.
+    API_FUNCTION() static void Unindent(float indentW = 0.0f)
+    {
+        ImGui::Unindent(indentW);
+    }
+
+    // Lock horizontal starting position.
+    API_FUNCTION() static void BeginGroup()
+    {
+        ImGui::BeginGroup();
+    }
+
+    // Unlock horizontal starting position + capture the whole group bounding box into one "item" (so you can use IsItemHovered() or layout primitives such as SameLine() on whole group, etc.).
+    API_FUNCTION() static void EndGroup()
+    {
+        ImGui::EndGroup();
+    }
+
+public:
+    // Push string into the ID stack (will hash string).
+    API_FUNCTION() static void PushID(const StringAnsiView& strID)
+    {
+        ImGui::PushID(strID.Get());
+    }
+
+    // Push integer into the ID stack (will hash integer).
+    API_FUNCTION() static void PushID(int32 intID)
+    {
+        ImGui::PushID(intID);
+    }
+
+    // Pops ID from stack.
+    API_FUNCTION() static void PopID()
+    {
+        ImGui::PopID();
+    }
+
+public:
+    // Text.
+    API_FUNCTION() static void Text(const StringAnsiView& text)
+    {
+        ImGui::Text(text.GetText());
+    }
+
+    // Shortcut for PushStyleColor(ImGuiCol_Text, col); Text(text); PopStyleColor();
+    API_FUNCTION() static void TextColored(const Color& col, const StringAnsiView& text)
+    {
+        ImGui::TextColored(*(ImVec4*)&col, text.GetText());
+    }
+
+    // Shortcut for PushStyleColor(ImGuiCol_Text, style.Colors[ImGuiCol_TextDisabled]); Text(text); PopStyleColor();
+    API_FUNCTION() static void TextDisabled(const StringAnsiView& text)
+    {
+        ImGui::TextDisabled(text.GetText());
+    }
+
+    // Shortcut for PushTextWrapPos(0.0f); Text(text); PopTextWrapPos();. Note that this won't work on an auto-resizing window if there's no other widgets to extend the window width, yoy may need to set a size using SetNextWindowSize().
+    API_FUNCTION() static void TextWrapped(const StringAnsiView& text)
+    {
+        ImGui::TextWrapped(text.GetText());
+    }
+
+    // Display text+label aligned the same way as value+label widgets.
+    API_FUNCTION() static void LabelText(const StringAnsiView& label, const StringAnsiView& text)
+    {
+        ImGui::LabelText(label.GetText(), text.GetText());
+    }
+
+    // Shortcut for Bullet()+Text().
+    API_FUNCTION() static void BulletText(const StringAnsiView& text)
+    {
+        ImGui::BulletText(text.GetText());
+    }
+
+public:
+    // Button.
+    API_FUNCTION() static bool Button(const StringAnsiView& label, const Float2& size = Float2::Zero)
+    {
+        return ImGui::Button(label.GetText(), *(ImVec2*)&size);
+    }
+
+    // Button with FramePadding=(0,0) to easily embed within text.
+    API_FUNCTION() static bool SmallButton(const StringAnsiView& label)
+    {
+        return ImGui::SmallButton(label.GetText());
+    }
+
+    // Square button with an arrow shape.
+    API_FUNCTION() static bool ArrowButton(const StringAnsiView& str, ImGuiDir dir)
+    {
+        return ImGui::ArrowButton(str.GetText(), dir);
+    }
+
+    // Image.
+    API_FUNCTION() static void Image(GPUTexture* texture, const Float2& size, const Float2& uv0 = Float2::Zero, const Float2& uv1 = Float2::One, const Color& tintColor = Color::White, const Color& borderColor = Color::Transparent)
+    {
+        ImGui::Image((ImTextureID)texture, *(ImVec2*)&size, *(ImVec2*)&uv0, *(ImVec2*)&uv1, *(ImVec4*)&tintColor, *(ImVec4*)&borderColor);
+    }
+
+    // less than 0 framePadding uses default frame padding settings. 0 for no padding.
+    API_FUNCTION() static bool ImageButton(GPUTexture* texture, const Float2& size, const Float2& uv0 = Float2::Zero, const Float2& uv1 = Float2::One, const Color& backgroundColor = Color::Transparent, const Color& tintColor = Color::White, int32 framePadding = -1)
+    {
+        return ImGui::ImageButton((ImTextureID)texture, *(ImVec2*)&size, *(ImVec2*)&uv0, *(ImVec2*)&uv1, framePadding, *(ImVec4*)&backgroundColor, *(ImVec4*)&tintColor);
+    }
+
+    // Checkbox.
+    API_FUNCTION() static bool Checkbox(const StringAnsiView& label, bool* v)
+    {
+        return ImGui::Checkbox(label.GetText(), v);
+    }
+
+    // Checkbox for flags value.
+    API_FUNCTION() static bool CheckboxFlags(const StringAnsiView& label, API_PARAM(Ref) int32& flags, int32 flagsValue)
+    {
+        return ImGui::CheckboxFlags(label.GetText(), &flags, flagsValue);
+    }
+
+    // Checkbox for flags value.
+    API_FUNCTION() static bool CheckboxFlags(const StringAnsiView& label, API_PARAM(Ref) uint32& flags, uint32 flagsValue)
+    {
+        return ImGui::CheckboxFlags(label.GetText(), &flags, flagsValue);
+    }
+
+    // Use with e.g. if (RadioButton("one", my_value==1)) { my_value = 1; }
+    API_FUNCTION() static bool RadioButton(const StringAnsiView& label, bool active)
+    {
+        return ImGui::RadioButton(label.GetText(), active);
+    }
+
+    // shortcut to handle the above pattern when value is an integer
+    API_FUNCTION() static bool RadioButton(const StringAnsiView& label, API_PARAM(Ref) int32& v, int vButton)
+    {
+        return ImGui::RadioButton(label.GetText(), &v, vButton);
+    }
+
+    // ProgressBar.
+    API_FUNCTION() static void ProgressBar(float fraction, const Float2& size, const StringAnsiView& overlay = StringAnsiView::Empty)
+    {
+        ImGui::ProgressBar(fraction, *(ImVec2*)&size, overlay.GetText());
+    }
+
+    // draw a small circle + keep the cursor on the same line. advance cursor x position by GetTreeNodeToLabelSpacing(), same distance that TreeNode() uses
+    API_FUNCTION() static void Bullet()
+    {
+        ImGui::Bullet();
+    }
+
+public:
+    // Combo.
+    API_FUNCTION() static bool Combo(const StringAnsiView& label, API_PARAM(Ref) int32& currentItem, const Array<StringAnsi>& items, int itemsCount = -1, int popupMaxHeightInItems = -1)
+    {
+        Array<const char*> itemsRaw;
+        itemsRaw.Resize(items.Count());
+        for (int32 i = 0; i < items.Count(); i++)
+            itemsRaw[i] = items[i].GetText();
+        if (itemsCount == -1)
+            itemsCount = items.Count();
+        return ImGui::Combo(label.GetText(), &currentItem, itemsRaw.Get(), itemsCount, popupMaxHeightInItems);
+    }
+
+    // Separate items with \0 within a string, end item-list with \0\0. e.g. "One\0Two\0Three\0"
+    API_FUNCTION() static bool Combo(const StringAnsiView& label, API_PARAM(Ref) int32& currentItem, const StringAnsiView& itemsSeparatedByZeros, int popupMaxHeightInItems = -1)
+    {
+        return ImGui::Combo(label.GetText(), &currentItem, itemsSeparatedByZeros.Get(), popupMaxHeightInItems);
+    }
+
+public:
+    // If vMin >= vMax we have no bound
+    API_FUNCTION() static bool DragFloat(const StringAnsiView& label, API_PARAM(Ref) float& v, float vSpeed = 1.0f, float vMin = 0.0f, float vMax = 0.0f, const StringAnsiView& format = "%.3f", ImGuiSliderFlags flags = 0)
+    {
+        return ImGui::DragFloat(label.GetText(), &v, vSpeed, vMin, vMax, format.GetText(), flags);
+    }
+
+    // If vMin >= vMax we have no bound
+    API_FUNCTION() static bool DragFloat2(const StringAnsiView& label, API_PARAM(Ref) Float2& v, float vSpeed = 1.0f, float vMin = 0.0f, float vMax = 0.0f, const StringAnsiView& format = "%.3f", ImGuiSliderFlags flags = 0)
+    {
+        return ImGui::DragFloat2(label.GetText(), (float*)&v, vSpeed, vMin, vMax, format.GetText(), flags);
+    }
+
+    // If vMin >= vMax we have no bound
+    API_FUNCTION() static bool DragFloat3(const StringAnsiView& label, API_PARAM(Ref) Float3& v, float vSpeed = 1.0f, float vMin = 0.0f, float vMax = 0.0f, const StringAnsiView& format = "%.3f", ImGuiSliderFlags flags = 0)
+    {
+        return ImGui::DragFloat3(label.GetText(), (float*)&v, vSpeed, vMin, vMax, format.GetText(), flags);
+    }
+
+    // If vMin >= vMax we have no bound
+    API_FUNCTION() static bool DragFloat4(const StringAnsiView& label, API_PARAM(Ref) Float4& v, float vSpeed = 1.0f, float vMin = 0.0f, float vMax = 0.0f, const StringAnsiView& format = "%.3f", ImGuiSliderFlags flags = 0)
+    {
+        return ImGui::DragFloat4(label.GetText(), (float*)&v, vSpeed, vMin, vMax, format.GetText(), flags);
+    }
+
+    // If vMin >= vMax we have no bound
+    API_FUNCTION() static bool DragInt(const StringAnsiView& label, API_PARAM(Ref) int32& v, float vSpeed = 1.0f, int vMin = 0, int vMax = 0, const StringAnsiView& format = "%d", ImGuiSliderFlags flags = 0)
+    {
+        return ImGui::DragInt(label.GetText(), &v, vSpeed, vMin, vMax, format.GetText(), flags);
+    }
+
+public:
+    // adjust format to decorate the value with a prefix or a suffix for in-slider labels or unit display.
+    API_FUNCTION() static bool SliderFloat(const StringAnsiView& label, API_PARAM(Ref) float& v, float vMin, float vMax, const StringAnsiView& format = "%.3f", ImGuiSliderFlags flags = 0)
+    {
+        return ImGui::SliderFloat(label.GetText(), &v, vMin, vMax, format.GetText(), flags);
+    }
+
+    // adjust format to decorate the value with a prefix or a suffix for in-slider labels or unit display.
+    API_FUNCTION() static bool SliderFloat2(const StringAnsiView& label, API_PARAM(Ref) Float2& v, float vMin, float vMax, const StringAnsiView& format = "%.3f", ImGuiSliderFlags flags = 0)
+    {
+        return ImGui::SliderFloat2(label.GetText(), (float*)&v, vMin, vMax, format.GetText(), flags);
+    }
+
+    // adjust format to decorate the value with a prefix or a suffix for in-slider labels or unit display.
+    API_FUNCTION() static bool SliderFloat3(const StringAnsiView& label, API_PARAM(Ref) Float3& v, float vMin, float vMax, const StringAnsiView& format = "%.3f", ImGuiSliderFlags flags = 0)
+    {
+        return ImGui::SliderFloat3(label.GetText(), (float*)&v, vMin, vMax, format.GetText(), flags);
+    }
+
+    // adjust format to decorate the value with a prefix or a suffix for in-slider labels or unit display.
+    API_FUNCTION() static bool SliderFloat4(const StringAnsiView& label, API_PARAM(Ref) Float4& v, float vMin, float vMax, const StringAnsiView& format = "%.3f", ImGuiSliderFlags flags = 0)
+    {
+        return ImGui::SliderFloat4(label.GetText(), (float*)&v, vMin, vMax, format.GetText(), flags);
+    }
+
+    // adjust format to decorate the value with a prefix or a suffix for in-slider labels or unit display.
+    API_FUNCTION() static bool SliderAngle(const StringAnsiView& label, API_PARAM(Ref) float& vRad, float vDegreesMin = -360.0f, float vDegreesMax = +360.0f, const StringAnsiView& format = "%.0f deg", ImGuiSliderFlags flags = 0)
+    {
+        return ImGui::SliderAngle(label.GetText(), &vRad, vDegreesMin, vDegreesMax, format.GetText(), flags);
+    }
+
+    // adjust format to decorate the value with a prefix or a suffix for in-slider labels or unit display.
+    API_FUNCTION() static bool SliderInt(const StringAnsiView& label, API_PARAM(Ref) int32& v, int vMin, int vMax, const StringAnsiView& format = "%d", ImGuiSliderFlags flags = 0)
+    {
+        return ImGui::SliderInt(label.GetText(), &v, vMin, vMax, format.GetText(), flags);
+    }
+
+public:
+    // Input text field.
+    API_FUNCTION() static bool InputText(const StringAnsiView& label, API_PARAM(Ref) StringAnsi& str, ImGuiInputTextFlags flags = 0)
+    {
+        ASSERT((flags & ImGuiInputTextFlags_CallbackResize) == 0);
+        flags |= ImGuiInputTextFlags_CallbackResize;
+        InputTextCallbackUserData textUserData;
+        textUserData.Str = &str;
+        return ImGui::InputText(label.GetText(), (char*)str.GetText(), str.Length() + 1, flags, InputTextCallback, &textUserData);
+    }
+
+    // Input multi-line text field.
+    API_FUNCTION() static bool InputTextMultiline(const StringAnsiView& label, API_PARAM(Ref) StringAnsi& str, const Float2& size, ImGuiInputTextFlags flags = 0)
+    {
+        ASSERT((flags & ImGuiInputTextFlags_CallbackResize) == 0);
+        flags |= ImGuiInputTextFlags_CallbackResize;
+        InputTextCallbackUserData textUserData;
+        textUserData.Str = &str;
+        return ImGui::InputTextMultiline(label.GetText(), (char*)str.GetText(), str.Length() + 1, *(ImVec2*)&size, flags, InputTextCallback, &textUserData);
+    }
+
+private:
+    struct InputTextCallbackUserData
+    {
+        StringAnsi* Str;
+    };
+
+    static int InputTextCallback(ImGuiInputTextCallbackData* data)
+    {
+        // Reference: https://github.com/ocornut/imgui/blob/master/misc/cpp/imgui_stdlib.cpp
+        auto userData = (InputTextCallbackUserData*)data->UserData;
+        if (data->EventFlag == ImGuiInputTextFlags_CallbackResize)
+        {
+            // Resize string
+            StringAnsi* str = userData->Str;
+            str->Resize(data->BufTextLen);
+            data->Buf = (char*)str->Get();
+        }
+        return 0;
+    }
+};
